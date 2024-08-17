f179023baa9d8090b52927a45867d874fa9f15cc
Devisowmya-547 <115868465+Devisowmya-547@users.noreply.github.com>
2024-08-17 22:06:19 +0530

    Add files via upload

--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -10,16 +10,18 @@ function Task() {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
   const [deadline, setDeadline] = useState("");
+  const [completed, setCompleted] = useState();
   const arr = [
-    {title : "Title", desc : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius nobis dolores facilis nulla quaerat placeat obcaecati aut quidem maiores, rem, beatae quisquam ipsum itaque nemo! Ab ducimus architecto quam quae.", deadline : "dd/mm/yyyy"},
-    {title : "Title", desc : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius nobis dolores facilis nulla quaerat placeat obcaecati aut quidem maiores, rem, beatae quisquam ipsum itaque nemo! Ab ducimus architecto quam quae.", deadline : "dd/mm/yyyy"},
-    {title : "Title", desc : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius nobis dolores facilis nulla quaerat placeat obcaecati aut quidem maiores, rem, beatae quisquam ipsum itaque nemo! Ab ducimus architecto quam quae.", deadline : "dd/mm/yyyy"}
+    {title : "Title", desc : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius nobis dolores facilis nulla quaerat placeat obcaecati aut quidem maiores, rem, beatae quisquam ipsum itaque nemo! Ab ducimus architecto quam quae.", completed : false, deadline : "dd/mm/yyyy"},
+    {title : "Title", desc : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius nobis dolores facilis nulla quaerat placeat obcaecati aut quidem maiores, rem, beatae quisquam ipsum itaque nemo! Ab ducimus architecto quam quae.", completed : false, deadline : "dd/mm/yyyy"},
+    {title : "Title", desc : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius nobis dolores facilis nulla quaerat placeat obcaecati aut quidem maiores, rem, beatae quisquam ipsum itaque nemo! Ab ducimus architecto quam quae.", completed : false, deadline : "dd/mm/yyyy"}
   ]
   const [taskFlag, setTaskFlag] = useState(arr.map(() => ({ edit: false })));
   const toggleEdit = (index, task) => {
       setTitle(task.title)
       setDesc(task.desc)
       setDeadline(task.deadline)
+      setCompleted(task.completed)
     setTaskFlag(prevFlags => 
       prevFlags.map((flag, i) => 
         i === index ? { ...flag, edit: true} : flag
@@ -44,6 +46,7 @@ function Task() {
                   </div>
                 </div>
                 {taskFlag[index].edit ? <textarea type='textarea' id='desc' value={desc} required onChange={(e) => {setDesc(e.target.value)}}/> : <p>{task.desc}</p>}
+                {taskFlag[index].edit ? <div><label htmlFor="status">Finished </label><input type='checkbox' id='status' checked={completed} onChange={(e) => {setCompleted(!completed)}} /></div> : <p>status : {task.completed ? "Finished" : "Not finished"}</p>}
                 {taskFlag[index].edit ? <input type='text' id='deadline' value={deadline} required onChange={(e) => {setDeadline(e.target.value)}}/> : <p>deadline : {task.deadline}</p>}
               </form>
               </div>              
